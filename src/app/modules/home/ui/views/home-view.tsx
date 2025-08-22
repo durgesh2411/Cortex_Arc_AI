1cb571bf9c570fc7d77222e21f41d77789725405
Durgesh Kumar Singh <durgesh.vns24@gmail.com>
2025-08-22 17:29:05 +0530

    08:trpc-setup

--- src/app/modules/home/ui/views/home-view.tsx
+++ src/app/modules/home/ui/views/home-view.tsx
@@ -2,30 +2,16 @@
 import { Button } from "@/components/ui/button";
 import {authClient} from "@/lib/auth-client";
 import {useRouter} from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 
 export const HomeView = ()=>{
-   const {data: session} = authClient.useSession();
-   const router = useRouter();
-   if(!session){
-      return(
-         <p>Loading!....</p>
-      )
-   }
+   const trpc  = useTRPC()
+   const { data } = useQuery(trpc.hello.queryOptions({text: "Durgesh"}))
    return (
-      <div>
-         <div className="flex flex-col p-4 gap-y-4">
-            <p>Logged in as {session.user.name}</p>
-            <Button onClick={
-               ()=>{
-                  authClient.signOut({
-                     fetchOptions:{
-                        onSuccess:() => router.push("/sign-in")
-                     }
-                  })
-               }
-            }>SignOut</Button>
-         </div>
-
+      <div className="flex flex-col p-4 gap-y-4">
+         {data?.greeting}
       </div>
    )
 }
